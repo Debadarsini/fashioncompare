167c57e3a8b5db8840daa4ab82a89689d1ccf43a
Debadarsini.Nayak <Debadarsini.Nayak@jasper.com>
2017-10-09 16:20:17 +0530

    read me files

--- app/src/main/java/com/example/debnayak/fashioncompare/MainActivity.java
+++ app/src/main/java/com/example/debnayak/fashioncompare/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +13,16 @@ public class MainActivity extends AppCompatActivity {
 
     private ActionBarDrawerToggle actionBarDrawerToggle;
 
+    private Toolbar mToolBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
         actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        mToolBar = (Toolbar)findViewById(R.id.nav_action);
+        setSupportActionBar(mToolBar);
 
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
