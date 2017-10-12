56b9d6a431d6107bbd035a151833cbb927b4085d
Debadarsini.Nayak <Debadarsini.Nayak@jasper.com>
2017-10-12 10:02:41 +0530

    google sign in

--- app/src/main/java/com/example/debnayak/fashioncompare/MainActivity.java
+++ app/src/main/java/com/example/debnayak/fashioncompare/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.debnayak.fashioncompare;
 
+import android.support.design.internal.NavigationMenu;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private ActionBarDrawerToggle actionBarDrawerToggle;
 
     private Toolbar mToolBar;
+
+    private TextView navigationText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,12 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        View headerLayout = navigationView.getHeaderView(0);
+        navigationText =(TextView) headerLayout.findViewById(R.id.navigation_header);
+        String displayName = (String) getIntent().getExtras().get("account");
+        navigationText.setText("Hello "+displayName.substring(0,11));
+
     }
 
     @Override
